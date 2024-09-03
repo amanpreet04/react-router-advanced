2b868d8ea5b92a2a6d01e8c9197d2b4514b7d665
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-09-03 23:51:12 +0530

    Added links to events

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function EventsPage() {
-  return <h1>Events Page</h1>;
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        <li>
+          <Link to="1">Event 1</Link>
+        </li>
+        <li>
+          <Link to="2">Event 2</Link>
+        </li>
+        <li>
+          <Link to="3">Event 3</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
