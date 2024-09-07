a22be800d00c68e3ca912206866d3e9ec3f0f69b
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-09-07 20:18:28 +0530

    Using data from loader in the route component

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,20 +1,9 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
-export default function EventsPage() {
-  return (
-    <>
-      <h1>Events Page</h1>
-      <ul>
-        <li>
-          <Link to="1">Event 1</Link>
-        </li>
-        <li>
-          <Link to="2">Event 2</Link>
-        </li>
-        <li>
-          <Link to="3">Event 3</Link>
-        </li>
-      </ul>
-    </>
-  );
+function EventsPage() {
+  const events = useLoaderData();
+  return <>{<EventsList events={events} />}</>;
 }
+
+export default EventsPage;
