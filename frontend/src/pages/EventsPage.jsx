fd6a27c72769ec4b8e20e10711702630e212997c
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-09-08 21:46:53 +0530

    Sending Response object from Loader

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,15 +1,17 @@
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  return <>{<EventsList />}</>;
+  return <EventsList />;
 }
 
 export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: "Some suitable error message" })
+    );
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 };
 
