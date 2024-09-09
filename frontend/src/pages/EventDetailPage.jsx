08c585ec8ddc3fa91013e5001bd59046deca80ef
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-09-09 22:18:32 +0530

    Added loaded fn for EventDetailsPage

--- frontend/src/pages/EventDetailPage.jsx
+++ frontend/src/pages/EventDetailPage.jsx
@@ -1,11 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 export default function EventDetailsPage() {
-  const params = useParams();
-  return (
-    <>
-      <h1>Event Details Page</h1>
-      <h2>{`Event ${params.eventId}`}</h2>
-    </>
-  );
+  const data = useLoaderData();
+  return <EventItem event={data.event} />;
+}
+
+export async function loaderForEventDetails({ request, params }) {
+  const eventId = params.eventId;
+  const url = `http://localhost:8080/events/${eventId}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: "Could not fetch the requested event" })
+    );
+  } else {
+    const data = await response.json();
+    return data;
+  }
 }
