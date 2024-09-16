76e5ca9af133822fd0a3d598c969db8f67e141ea
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-09-16 21:41:31 +0530

    Handled creation of new event

--- frontend/src/pages/NewEventPage.jsx
+++ frontend/src/pages/NewEventPage.jsx
@@ -1,3 +1,32 @@
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
 export default function NewEventPage() {
-  return <h1>New Event Page</h1>;
+  return <EventForm />;
 }
+
+export const action = async ({ request }) => {
+  const data = await request.formData();
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    body: JSON.stringify(eventData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  console.log(`response: ${response}`);
+
+  if (!response.ok) {
+    throw new Response({ message: "Couldnt save event" });
+  }
+
+  return redirect('/events')
+};
